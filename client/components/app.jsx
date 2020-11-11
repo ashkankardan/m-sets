ba40869dcf84999bb3d76fd025c7dc0d06acc6dd
Mason Barnes <hypnosismcb@yahoo.com>
2020-11-11 09:50:10 -0800

    Completed feature 'user can view sets' on home page

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from './header';
 import Footer from './footer';
+import SetsList from './sets-list';
+import ArtistsList from './artists-list';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -24,18 +26,28 @@ export default class App extends React.Component {
     let viewElement;
     if (this.state.isLoading) {
       viewElement = <h1>Testing connections...</h1>;
+      return viewElement;
     } else if (!this.state.isLoading) {
-      viewElement = <div>
-        <h1>It works</h1>
+      viewElement = <div className="center">
         <h1>{this.state.message.toUpperCase()}</h1>
       </div>;
     }
-    return (
-      <div>
-        <Header />
-        {viewElement}
-        <Footer />
-      </div>
-    );
+    if (this.state.view === 'home') {
+      return (
+        <div className="container">
+          <Header />
+          <SetsList />
+          <ArtistsList />
+          <Footer />
+        </div>
+      );
+    } else {
+      return (
+        <div className="container">
+          <Header />
+          <Footer />
+        </div>
+      );
+    }
   }
 }
